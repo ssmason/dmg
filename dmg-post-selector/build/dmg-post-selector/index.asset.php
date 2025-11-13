cd9a1116bbd5f78dc9f9307f5e657147b1e3d828
stephen mason <stephenmason@stephens-MBP.OleLan>
2025-11-13 13:33:51 +0100

    chore: update pointer on posts hover and background-color

--- dmg-post-selector/build/dmg-post-selector/index.asset.php
+++ dmg-post-selector/build/dmg-post-selector/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a48d6f5f312886fe1faa');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e726220d6eaa50e4d6c9');
